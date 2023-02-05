0fac9f15ddbbed44ed86ff9339c3ecea9edc4206
YOUR NAME <jhanov@gmail.com>
2023-02-05 07:24:13 -0600

    add flag img url to seed data

--- app/models/tournament_group.rb
+++ app/models/tournament_group.rb
@@ -1,4 +1,20 @@
 class TournamentGroup < ApplicationRecord
   belongs_to :tournament
   has_many :teams
+
+  def group_winners
+    winner = []
+    current_highest_points = 0
+    group_teams = Team.where(tournament_group_id: id)
+    group_teams.each do |team|
+      points = team.total_points
+
+      if points > current_highest_points
+        current_highest_points = points
+        winner = team
+      end
+
+      winner
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,6 @@ Tournament.destroy_all
 
 tournament = FactoryBot.create(:tournament)
 
-
 tournament.tournament_groups.create(
   [
     {
@@ -31,17 +30,18 @@ tournament.tournament_groups.create(
     },
   ]
 )
-FactoryBot.create(:team, 
-  :argentina, 
-  seed_rank: 1, 
-  tournament_group: TournamentGroup.find_by(name: "C"),
-)
+FactoryBot.create(:team,
+                  :argentina,
+                  seed_rank: 1,
+                  flag_img_url: "https://olsworldflag.files.wordpress.com/2009/11/flag-of-argentina.gif?w=300&h=150&zoom=2",
+                  tournament_group: TournamentGroup.find_by(name: "C"))
 
 Team.create(
   name: "Poland",
   seed_rank: 3,
   jersey_primary: "White",
   jersey_secondary: "Red",
+  flag_img_url: "https://aceproject.org/archived-content/root/PL_flag.gif/image_preview",
   tournament_group: TournamentGroup.find_by(name: "C"),
 )
 
@@ -50,6 +50,7 @@ Team.create(
   seed_rank: 2,
   jersey_primary: "Green",
   jersey_secondary: "Red",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/en/2/20/Flag_of_Mexico_1917.png?20060117062247",
   tournament_group: TournamentGroup.find_by(name: "C"),
 )
 
@@ -58,6 +59,7 @@ Team.create(
   seed_rank: 4,
   jersey_primary: "Green",
   jersey_secondary: "White",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Flag_of_Saudi_Arabia.svg/1920px-Flag_of_Saudi_Arabia.svg.png",
   tournament_group: TournamentGroup.find_by(name: "C"),
 )
 
@@ -66,6 +68,7 @@ Team.create(
   seed_rank: 1,
   jersey_primary: "Orange",
   jersey_secondary: "White",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/1920px-Flag_of_the_Netherlands.svg.png",
   tournament_group: TournamentGroup.find_by(name: "A"),
 )
 
@@ -74,6 +77,7 @@ Team.create(
   seed_rank: 2,
   jersey_primary: "White",
   jersey_secondary: "Green",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Flag_of_Senegal.svg/1920px-Flag_of_Senegal.svg.png",
   tournament_group: TournamentGroup.find_by(name: "A"),
 )
 
@@ -82,6 +86,7 @@ Team.create(
   seed_rank: 3,
   jersey_primary: "Yellow",
   jersey_secondary: "Blue",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Flag_of_Ecuador.svg/250px-Flag_of_Ecuador.svg.png",
   tournament_group: TournamentGroup.find_by(name: "A"),
 )
 
@@ -90,6 +95,7 @@ Team.create(
   seed_rank: 4,
   jersey_primary: "Red",
   jersey_secondary: "White",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Flag_of_Qatar.svg/2560px-Flag_of_Qatar.svg.png",
   tournament_group: TournamentGroup.find_by(name: "A"),
 )
 
@@ -98,6 +104,7 @@ Team.create(
   seed_rank: 1,
   jersey_primary: "White",
   jersey_secondary: "Red",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Flag_of_England.svg/1600px-Flag_of_England.svg.png?20111003040319",
   tournament_group: TournamentGroup.find_by(name: "B"),
 )
 
@@ -106,6 +113,7 @@ Team.create(
   seed_rank: 2,
   jersey_primary: "Blue",
   jersey_secondary: "White",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1600px-Flag_of_the_United_States.svg.png?20151118161041",
   tournament_group: TournamentGroup.find_by(name: "B"),
 )
 
@@ -114,6 +122,7 @@ Team.create(
   seed_rank: 4,
   jersey_primary: "Green",
   jersey_secondary: "White",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Flag_of_Iran.svg/250px-Flag_of_Iran.svg.png",
   tournament_group: TournamentGroup.find_by(name: "B"),
 )
 
@@ -121,6 +130,7 @@ Team.create(
   name: "Wales",
   seed_rank: 3,
   jersey_primary: "Red",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Flag_of_Wales.svg/250px-Flag_of_Wales.svg.png",
   jersey_secondary: "White",
   tournament_group: TournamentGroup.find_by(name: "B"),
 )
@@ -129,6 +139,7 @@ Team.create(
   name: "France",
   seed_rank: 1,
   jersey_primary: "Blue",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/1599px-Flag_of_France.svg.png?20220120162234",
   jersey_secondary: "White",
   tournament_group: TournamentGroup.find_by(name: "D"),
 )
@@ -138,6 +149,7 @@ Team.create(
   seed_rank: 4,
   jersey_primary: "Yellow",
   jersey_secondary: "Blue",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Flag_of_Australia_%28converted%29.svg/2560px-Flag_of_Australia_%28converted%29.svg.png",
   tournament_group: TournamentGroup.find_by(name: "D"),
 )
 
@@ -146,6 +158,7 @@ Team.create(
   seed_rank: 3,
   jersey_primary: "Red",
   jersey_secondary: "White",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Tunisia.svg/1920px-Flag_of_Tunisia.svg.png",
   tournament_group: TournamentGroup.find_by(name: "D"),
 )
 
@@ -154,6 +167,7 @@ Team.create(
   seed_rank: 2,
   jersey_primary: "Red",
   jersey_secondary: "White",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Flag_of_Denmark.svg/1920px-Flag_of_Denmark.svg.png",
   tournament_group: TournamentGroup.find_by(name: "D"),
 )
 
@@ -162,6 +176,7 @@ Team.create(
   seed_rank: 1,
   jersey_primary: "Blue",
   jersey_secondary: "White",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/1599px-Flag_of_Japan.svg.png?20111003030759",
   tournament_group: TournamentGroup.find_by(name: "E"),
 )
 
@@ -170,6 +185,7 @@ Team.create(
   seed_rank: 1,
   jersey_primary: "Red",
   jersey_secondary: "White",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1500px-Bandera_de_Espa%C3%B1a.svg.png",
   tournament_group: TournamentGroup.find_by(name: "E"),
 )
 
@@ -178,14 +194,16 @@ Team.create(
   seed_rank: 2,
   jersey_primary: "White",
   jersey_secondary: "Blue",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1920px-Flag_of_Germany.svg.png",
   tournament_group: TournamentGroup.find_by(name: "E"),
 )
 
 Team.create(
   name: "Costa Rica",
-  seed_rank: 4,x
+  seed_rank: 4,
   jersey_primary: "Red",
   jersey_secondary: "White",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Costa_Rica.svg/1920px-Flag_of_Costa_Rica.svg.png",
   tournament_group: TournamentGroup.find_by(name: "E"),
 )
 
@@ -194,6 +212,7 @@ Team.create(
   seed_rank: 3,
   jersey_primary: "Red",
   jersey_secondary: "White",
+  flag_img_url: "https://en.wikipedia.org/wiki/Morocco#/media/File:Flag_of_Morocco.svg",
   tournament_group: TournamentGroup.find_by(name: "F"),
 )
 
@@ -202,6 +221,7 @@ Team.create(
   seed_rank: 2,
   jersey_primary: "White/Red",
   jersey_secondary: "Blue",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Flag_of_Croatia.svg/250px-Flag_of_Croatia.svg.png",
   tournament_group: TournamentGroup.find_by(name: "F"),
 )
 
@@ -209,6 +229,7 @@ Team.create(
   name: "Belgium",
   seed_rank: 1,
   jersey_primary: "Red",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Flag_of_Belgium.svg/1280px-Flag_of_Belgium.svg.png",
   jersey_secondary: "Red",
   tournament_group: TournamentGroup.find_by(name: "F"),
 )
@@ -216,6 +237,7 @@ Team.create(
 Team.create(
   name: "Canada",
   seed_rank: 3,
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/2560px-Flag_of_Canada_%28Pantone%29.svg.png",
   jersey_primary: "White",
   jersey_secondary: "Red",
   tournament_group: TournamentGroup.find_by(name: "F"),
@@ -226,12 +248,14 @@ Team.create(
   seed_rank: 1,
   jersey_primary: "Yellow",
   jersey_secondary: "Blue",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/1440px-Flag_of_Brazil.svg.png?20111003040251",
   tournament_group: TournamentGroup.find_by(name: "G"),
 )
 
 Team.create(
   name: "Switzerland",
   seed_rank: 2,
+  flag_Img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Flag_of_Switzerland_%28Pantone%29.svg/1024px-Flag_of_Switzerland_%28Pantone%29.svg.png",
   jersey_primary: "Red",
   jersey_secondary: "White",
   tournament_group: TournamentGroup.find_by(name: "G"),
@@ -241,6 +265,7 @@ Team.create(
   name: "Cameroon",
   seed_rank: 3,
   jersey_primary: "Green",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Flag_of_Cameroon.svg/1920px-Flag_of_Cameroon.svg.png",
   jersey_secondary: "Red",
   tournament_group: TournamentGroup.find_by(name: "G"),
 )
@@ -250,6 +275,7 @@ Team.create(
   seed_rank: 4,
   jersey_primary: "Red",
   jersey_secondary: "White",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Flag_of_Serbia.svg/250px-Flag_of_Serbia.svg.png",
   tournament_group: TournamentGroup.find_by(name: "G"),
 )
 
@@ -257,6 +283,7 @@ Team.create(
   name: "Portugal",
   seed_rank: 1,
   jersey_primary: "Red",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/1200px-Flag_of_Portugal.svg.png",
   jersey_secondary: "Green",
   tournament_group: TournamentGroup.find_by(name: "H"),
 )
@@ -265,6 +292,7 @@ Team.create(
   name: "South Korea",
   seed_rank: 3,
   jersey_primary: "Blue",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Flag_of_South_Korea.svg/1920px-Flag_of_South_Korea.svg.png",
   jersey_secondary: "White",
   tournament_group: TournamentGroup.find_by(name: "H"),
 )
@@ -274,6 +302,7 @@ Team.create(
   seed_rank: 2,
   jersey_primary: "Sky Blue",
   jersey_secondary: "White",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Uruguay.svg/250px-Flag_of_Uruguay.svg.png",
   tournament_group: TournamentGroup.find_by(name: "H"),
 )
 
@@ -282,52 +311,50 @@ Team.create(
   seed_rank: 4,
   jersey_primary: "Green",
   jersey_secondary: "Yellow",
+  flag_img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Flag_of_Ghana.svg/1599px-Flag_of_Ghana.svg.png",
   tournament_group: TournamentGroup.find_by(name: "H"),
 )
 
 Stadium.create(
-  name: "Al Bayt", 
-  location: "Al Khor", 
-  capacity: 60000, 
+  name: "Al Bayt",
+  location: "Al Khor",
+  capacity: 60000,
 )
 
 Stadium.create(
-  name: "Education City", 
-  location: "Al Rayyan", 
-  capacity: 40000, 
-)
-
-
-Stadium.create(
-  name: "Stadium 974", 
-  location: "Doha", 
-  capacity: 40000, 
+  name: "Education City",
+  location: "Al Rayyan",
+  capacity: 40000,
 )
 
 Stadium.create(
-  name: "Khalifa International Stadium", 
-  location: "Doha", 
-  capacity: 40000, 
+  name: "Stadium 974",
+  location: "Doha",
+  capacity: 40000,
 )
 
 Stadium.create(
-  name: "Ahmed Bin Ali Stadium", 
-  location: "Umm Al Afaei", 
-  capacity: 40000, 
+  name: "Khalifa International Stadium",
+  location: "Doha",
+  capacity: 40000,
 )
 
 Stadium.create(
-  name: "Al Thummama Stadium", 
-  location: "Doha", 
-  capacity: 40000, 
+  name: "Ahmed Bin Ali Stadium",
+  location: "Umm Al Afaei",
+  capacity: 40000,
 )
 
 Stadium.create(
-  name: "Al Janoub", 
-  location: "Al Wakrah", 
-  capacity: 40000, 
+  name: "Al Thummama Stadium",
+  location: "Doha",
+  capacity: 40000,
 )
 
-Match.create(
-  
+Stadium.create(
+  name: "Al Janoub",
+  location: "Al Wakrah",
+  capacity: 40000,
 )
+
+Match.create()
