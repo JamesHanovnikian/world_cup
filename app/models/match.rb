11caa0c583ce0a261c8c0bf40b061cfa3ae4e521
YOUR NAME <jhanov@gmail.com>
2023-04-25 20:47:43 -0500

    adds tests for whether a match occurs outside of tournament dates or not

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,14 @@ class Match < ApplicationRecord
   belongs_to :home_team, class_name: "Team"
   belongs_to :away_team, class_name: "Team"
   belongs_to :tournament_group
+
+  validate :match_date_within_tournament
+
+  private
+
+  def match_date_within_tournament
+    unless date.between?(tournament_group.tournament.start_at, tournament_group.tournament.end_at)
+      errors.add(:date, "match date is outside tournament time")
+    end
+  end
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -3,9 +3,32 @@ require "rails_helper"
 RSpec.describe Match, type: :model do
   describe "match time" do
     let(:match) { FactoryBot.create(:match) }
+    let(:tournament) { match.tournament_group.tournament }
 
     it "has a date within the tournament times" do
-      expect(match.date).to be_between(Date.new(2022, 11, 20), Date.new(2022, 12, 13))
+      expect(match.date).to be_between(tournament.start_at, tournament.end_at)
+    end
+
+    context "when match occurs outside of tournament dates" do
+      let(:tournament) do
+        FactoryBot.create(
+          :tournament,
+          start_at: DateTime.new(2022, 5, 1, 12),
+          end_at: DateTime.new(2022, 6, 1, 12),
+        )
+      end
+      let(:tournament_group) { FactoryBot.create(:tournament_group, tournament: tournament) }
+      let(:match) do
+        FactoryBot.build(
+          :match,
+          date: DateTime.new(2022, 10, 1, 12),
+          tournament_group: tournament_group,
+        )
+      end
+
+      it "is invalid" do
+        expect(match).to be_invalid
+      end
     end
   end
 end
